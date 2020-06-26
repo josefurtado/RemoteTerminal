6315828a06747afe4f5e4c0b3d6c0ac4b6bc8d5e
José Furtado <josefurtado99@outlook.com>
2020-06-25 22:12:03 -0300

    add build project in batch file

--- BattleRoyalle/BattleRoyalle.Service/install.bat
+++ BattleRoyalle/BattleRoyalle.Service/install.bat
@@ -1,4 +1,5 @@
 set currentPath=%~dp0
 echo %currentPath%
+dotnet build %currentPath%
 START /WAIT %currentPath%\bin\Debug\netcoreapp3.1\BattleRoyalle.Service.exe install
 START /WAIT %currentPath%\bin\Debug\netcoreapp3.1\BattleRoyalle.Service.exe start
